ba153b919e312b6ae1ea335e43600ebc68e2287d
Yi Hou Loo <yihou@nixel.tech>
2020-06-04 02:31:39 +0800

    add .gitignore & .editorconfig & minor code improvements

--- src/IPay88.php
+++ src/IPay88.php
@@ -1,33 +1,40 @@
-<?php 
-use IPay88\RequestForm;
+<?php
+
+namespace App\Classes;
 
 class IPay88
 {
-	private $merchantKey = null;
-	public $merchantCode = null;
-	public $responseUrl = null;
-	public $backendResponseUrl = null;
+    private $merchantKey;
+    public $merchantCode;
+    public $responseUrl = null;
+    public $backendResponseUrl = null;
 
-	public function __construct($merchantKey, $merchantCode, $responseUrl, $backendResponseUrl)
-	{
-		$this->merchantKey = $merchantKey;
-		$this->merchantCode= $merchantCode;
-		$this->responseUrl = $responseUrl;
-		$this->backendResponseUrl = $backendResponseUrl;
-	}
-	
-	/**
+    /**
+     * IPay88 constructor.
+     * @param string $merchantKey Provided by iPay88 OPSG and share between iPay88 and merchant only
+     * @param string $merchantCode Merchant Code provided by iPay88 and use to uniquely identify the Merchant.
+     * @param string $responseUrl
+     * @param string $backendResponseUrl
+     */
+    public function __construct($merchantKey, $merchantCode, $responseUrl, $backendResponseUrl)
+    {
+        $this->merchantKey = $merchantKey;
+        $this->merchantCode= $merchantCode;
+        $this->responseUrl = $responseUrl;
+        $this->backendResponseUrl = $backendResponseUrl;
+    }
+
+    /**
      * Generate signature to be used for transaction.
      *
      * You may verify your signature with online tool provided by iPay88
      * http://www.mobile88.com/epayment/testing/TestSignature.asp
      *
      * @access public
-     * @param string $merchantKey ProvidedbyiPay88OPSGandsharebetweeniPay88and merchant only
-     * @param string $merchantCode Merchant Code provided by iPay88 and use to uniquely identify the Merchant.
      * @param string $refNo Unique merchant transaction id
      * @param int $amount Payment amount
      * @param string $currency Payment currency
+     * @return string
      */
     public function generateSignature($refNo, $amount, $currency)
     {
@@ -36,73 +43,76 @@ class IPay88
     }
 
     /**
-    *
-    * equivalent of php 5.4 hex2bin
-    *
-    * @access private
-    * @param string $source The string to be converted
-    */
+     *
+     * equivalent of php 5.4 hex2bin
+     *
+     * @access private
+     * @param string $source The string to be converted
+     * @return string|null
+     */
     private function _hex2bin($source)
     {
-    	$bin = null;
-    	for ($i=0; $i < strlen($source); $i=$i+2) { 
-    		$bin .= chr(hexdec(substr($source, $i, 2)));
-    	}
-    	return $bin;
+        $bin = null;
+        for ($i=0; $i < strlen($source); $i=$i+2) {
+            $bin .= chr(hexdec(substr($source, $i, 2)));
+        }
+        return $bin;
     }
 
     /**
-    * @access public
-    * @param boolean $multiCurrency Set to true to get payments optinos for multi currency gateway
-    */
+     * @access public
+     * @param boolean $multiCurrency Set to true to get payments optinos for multi currency gateway
+     * @return bool|string[][]
+     */
     public static function getPaymentOptions($multiCurrency = false)
     {
-        $myrOnly = array(
-        	2 => array('Credit Card','MYR'),
-        	6 => array('Maybank2U','MYR'),
-        	8 => array('Alliance Online','MYR'),
-        	10=> array('AmOnline','MYR'),
-        	14=> array('RHB Online','MYR'),
-        	15=> array('Hong Leong Online','MYR'),
-        	16=> array('FPX','MYR'),
-        	20=> array('CIMB Click', 'MYR'),
-        	22=> array('Web Cash','MYR'),
-        	48=> array('PayPal','MYR'),
-        	100 => array('Celcom AirCash','MYR'),
-        	102 => array('Bank Rakyat Internet Banking','MYR'),
-        	103 => array('AffinOnline','MYR')
-        	);
+        $myrOnly = [
+            2 => ['Credit Card','MYR'],
+            6 => ['Maybank2U','MYR'],
+            8 => ['Alliance Online','MYR'],
+            10=> ['AmOnline','MYR'],
+            14=> ['RHB Online','MYR'],
+            15=> ['Hong Leong Online','MYR'],
+            16=> ['FPX','MYR'],
+            20=> ['CIMB Click', 'MYR'],
+            22=> ['Web Cash','MYR'],
+            48=> ['PayPal','MYR'],
+            100 => ['Celcom AirCash','MYR'],
+            102 => ['Bank Rakyat Internet Banking','MYR'],
+            103 => ['AffinOnline','MYR']
+        ];
 
-        $multiCurrency = array(
-        	25=> array('Credit Card','USD'),
-        	35=> array('Credit Card','GBP'),
-        	36=> array('Credit Card','THB'),
-        	37=> array('Credit Card','CAD'),
-        	38=> array('Credit Card','SGD'),
-        	39=> array('Credit Card','AUD'),
-        	40=> array('Credit Card','MYR'),
-        	41=> array('Credit Card','EUR'),
-        	42=> array('Credit Card','HKD'),
-        	);
+        $multiCurrencyList = [
+            25=> ['Credit Card','USD'],
+            35=> ['Credit Card','GBP'],
+            36=> ['Credit Card','THB'],
+            37=> ['Credit Card','CAD'],
+            38=> ['Credit Card','SGD'],
+            39=> ['Credit Card','AUD'],
+            40=> ['Credit Card','MYR'],
+            41=> ['Credit Card','EUR'],
+            42=> ['Credit Card','HKD'],
+        ];
 
-        return $multiCurrency ? $multiCurrency : $myrOnly;
+        return $multiCurrency ? $multiCurrencyList : $myrOnly;
     }
 
     /**
-    * @access public
-    * @param 
-    */
+     * @access public
+     * @param $args
+     * @return mixed
+     */
     public function makeRequestForm($args)
     {
-    	$args['merchantCode'] = $this->merchantCode;
-    	$args['signature'] = $this->generateSignature(
-    		$args['refNo'],
-    		(int) $args['amount'],
-    		$args['currency']
-    		);
-    	$args['responseUrl'] = $this->responseUrl;
-    	$args['backendUrl'] = $this->backendResponseUrl;
+        $args['merchantCode'] = $this->merchantCode;
+        $args['signature'] = $this->generateSignature(
+            $args['refNo'],
+            (int) $args['amount'],
+            $args['currency']
+        );
+        $args['responseUrl'] = $this->responseUrl;
+        $args['backendUrl'] = $this->backendResponseUrl;
 
-        return new IPay88\RequestForm($args);
+        return $args;
     }
 }
